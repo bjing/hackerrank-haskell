d44ae50e60b201f420784951e23e1e7e383025eb
Brian Jing <brian.jing.dev@gmail.com>
2023-10-14 20:39:03 +1100

    Submit solution to ElectronicsShop problem

--- src/ElectronicsShop.hs
+++ src/ElectronicsShop.hs
@@ -15,48 +15,52 @@ import System.IO
 -- Complete the getMoneySpent function below.
 
 getMoneySpent :: [Int] -> [Int] -> Int -> Int
-getMoneySpent keyboards drives b = undefined
-    --
-    -- Write your code here.
-    --
+getMoneySpent keyboards drives b =
+  let allCombos = [(k, d) | k <- keyboards, d <- drives]
+      prices = Data.List.map (uncurry (+)) allCombos
+      pricesWithinBudget = Data.List.filter (<= b) prices
+  in
+    if pricesWithinBudget /= [] then
+        maximum pricesWithinBudget
+    else -1
 
 readMultipleLinesAsStringArray :: Int -> IO [String]
 readMultipleLinesAsStringArray 0 = return []
 readMultipleLinesAsStringArray n = do
-    line <- getLine
-    rest <- readMultipleLinesAsStringArray (n - 1)
-    return (line : rest)
+  line <- getLine
+  rest <- readMultipleLinesAsStringArray (n - 1)
+  return (line : rest)
 
 main :: IO()
 main = do
-    stdout <- getEnv "OUTPUT_PATH"
-    fptr <- openFile stdout WriteMode
+  stdout <- getEnv "OUTPUT_PATH"
+  fptr <- openFile stdout WriteMode
 
-    bnmTemp <- getLine
-    let bnm = words bnmTemp
+  bnmTemp <- getLine
+  let bnm = words bnmTemp
 
-    let b = read (head bnm) :: Int
+  let b = read (head bnm) :: Int
 
-    let n = read (bnm !! 1) :: Int
+  let n = read (bnm !! 1) :: Int
 
-    let m = read (bnm !! 2) :: Int
+  let m = read (bnm !! 2) :: Int
 
-    keyboardsTemp <- getLine
+  keyboardsTemp <- getLine
 
-    let keyboards = Data.List.map (read :: String -> Int) . words $ keyboardsTemp
+  let keyboards = Data.List.map (read :: String -> Int) . words $ keyboardsTemp
 
-    drivesTemp <- getLine
+  drivesTemp <- getLine
 
-    let drives = Data.List.map (read :: String -> Int) . words $ drivesTemp
+  let drives = Data.List.map (read :: String -> Int) . words $ drivesTemp
 
-    --
-    -- The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
-    --
+  --
+  -- The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+  --
 
-    let moneySpent = getMoneySpent keyboards drives b
+  let moneySpent = getMoneySpent keyboards drives b
 
-    hPrint fptr moneySpent
+  hPrint fptr moneySpent
 
-    hFlush fptr
-    hClose fptr
+  hFlush fptr
+  hClose fptr
 
